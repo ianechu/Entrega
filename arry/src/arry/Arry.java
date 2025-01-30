5a08522ba2705f974ac1ad07b2d7ce418c436176
ianechu <ianechu181005@gmail.com>
2025-01-30 09:12:13 +0100

    Para la entrega de entornos
    Signed_off_by: ianechu<ianechu181005@gmail.com>

---
+++ arry/src/arry/Arry.java
@@ -0,0 +1,21 @@
+package arry;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Arry {
+	final static int NUMERO_MAXIMO = 7;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		List<String> lista = new ArrayList<>();
+
+		for (int i = 0; i < NUMERO_MAXIMO; i++) {
+			System.out.println("Dime una palabra:");
+			String palabra = sc.next();
+			lista.add(palabra);
+			System.out.println("La palabra tiene " + lista.get(i).length() + " letras");
+		}
+	}
+}
